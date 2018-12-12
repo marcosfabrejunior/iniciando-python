c5a0dcb436c351b7fceebbce7b0ee800bc5a8e79
Marcos Fabre Jr <marcosfabrejunior@hotmail.com>
2018-12-12 10:59:43 -0200

    aula 5 - video 2

--- adivinhacao.py
+++ adivinhacao.py
@@ -10,13 +10,18 @@ rodada = 1
 # while(rodada <= total_tentativas):
 for rodada in range(1, total_tentativas + 1):
     print("Tentativa {} de {} ".format(rodada,total_tentativas))
-    chute = input("Digite um número: \n")
+    chute = input("Digite um número entre 1 e 100: \n")
     chute = int(chute)
+
+    if (chute < 1 or chute > 100):
+        print("Número fora da área de tentativas")
+        continue
 
     print("Você digitou ", chute)
 
     if(numero_secreto == chute):
         print("Você acertou")
+        break
     else: 
         if(chute > numero_secreto):
             print("Você errou, o seu chute foi maior que o número secreto")
