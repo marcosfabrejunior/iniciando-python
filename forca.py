ff9457b7ee9b87db84c0c1a7289a9d37a1d63646
Marcos Fabre Jr <marcosfabrejunior@hotmail.com>
2018-12-14 17:11:01 -0200

    Aula 2 - Concluído

--- forca.py
+++ forca.py
@@ -7,7 +7,20 @@ def jogar():
     enforcou = False
     acertou = False
 
+    palavra_secreta = palavra_secreta.lower()
+
     while (not enforcou and not acertou):
+        chute = input("Qual Letra?")
+        chute = chute.lower()
+        chute = chute.strip()
+
+        index = 0
+        for letra in palavra_secreta:
+            if(chute == letra):
+                
+
+            index = index + 1
+
         print("jogando...")
 
 
