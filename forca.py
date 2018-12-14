034cd8c8d3224573fe276c046396e54cc570e1fd
Marcos Fabre Jr <marcosfabrejunior@hotmail.com>
2018-12-14 15:23:08 -0200

    Aula 1 curso 2

--- forca.py
+++ forca.py
@@ -3,5 +3,13 @@ def jogar():
     print("*bem vindo ao jogo de forca*")
     print("**********************************")
 
+    palavra_secreta = "banana"
+    enforcou = False
+    acertou = False
+
+    while (not enforcou and not acertou):
+        print("jogando...")
+
+
 if(__name__ == "__main__"):
     jogar()
