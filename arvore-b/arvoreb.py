ae4e828741f9d572b054df985c8c9639a61fbe90
marcosfabrejr <marcosfabrejunior@hotmail.com>
2019-10-30 21:09:11 -0300

    arvore B

---
+++ arvore-b/arvoreb.py
@@ -0,0 +1,22 @@
+class pagina():
+    def __init__(self, ordem):
+        self.contador = 0
+        self.chave = []
+        self.filho = []
+        self.ordem = ordem
+    
+    def insert(self, chave):
+        if(self.contador == ordem):
+            
+            
+        else:
+            self.chave.push(1)
+            self.chave.sort()
+
+    
+   
+
+
+pagina = pagina(3)
+pagina.insert(1)
+
